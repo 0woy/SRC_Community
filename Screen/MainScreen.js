268404fd91e6dbfa165c86824491fefc5a89ddb3
0woy <qkrdbsdk1030@naver.com>
2023-04-02 15:28:42 +0900

    feat:add MainPage

---
+++ Screen/MainScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button, View, Text} from 'react-native';
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button title="go to the list screen" />
+    </View>
+  );
+};
+
+export default MainScreen;
