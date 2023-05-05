4fd13f4001f9abfbeb3c4c71ff6f58c17f96bf41
0woy <qkrdbsdk1030@naver.com>
2023-05-05 17:30:11 +0900

    feat:add navigation 전체게시판 page

--- Screen/Navigations/Stack.js
+++ Screen/Navigations/Stack.js
@@ -6,6 +6,7 @@ import MainScreen from '../MainScreen';
 import FindUserScreen from '../FindUserScreen';
 import FreeBoard from '../SubScreen/FreeBoard';
 import WriteBoard from '../SubScreen/WriteBorard';
+import AllBoard from '../SubScreen/AllBoard';
 import {NavigationContainer} from '@react-navigation/native';
 
 const Stack = createStackNavigator();
@@ -18,18 +19,24 @@ const StackNavigation = () => {
         name="로그인"
         component={SignInScreen}
       />
-
-      <Stack.Screen name="회원가입" component={SignUpScreen} />
-      <Stack.Screen name="아이디/비밀번호찾기" component={FindUserScreen} />
       <Stack.Screen
         name="메인화면"
         options={{headerShown: false, gestureEnabled: false}}
         component={MainScreen}
       />
+
+      <Stack.Screen name="회원가입" component={SignUpScreen} />
+      <Stack.Screen name="아이디/비밀번호찾기" component={FindUserScreen} />
+
       <Stack.Screen
         name="자유게시판"
         options={{headerShown: false, gestureEnabled: false}}
         component={FreeBoard}
+      />
+      <Stack.Screen
+        name="전체게시판"
+        options={{headerShown: false, gestureEnabled: false}}
+        component={AllBoard}
       />
       <Stack.Screen
         name="글쓰기"
