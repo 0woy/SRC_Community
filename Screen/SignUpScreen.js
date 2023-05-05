7c9201404422e8f56accaf2f4192bce08a2781c8
0woy <qkrdbsdk1030@naver.com>
2023-05-05 12:41:11 +0900

    feat:add SingUp page & store user info to firebase

--- Screen/SignUpScreen.js
+++ Screen/SignUpScreen.js
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
-import {View, TextInput, Button, Alert, Text, StyleSheet} from 'react-native';
+import {View, TextInput, Alert, Text, StyleSheet} from 'react-native';
 import auth from '@react-native-firebase/auth';
-import {signUp} from '../lib/auth';
-
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import styles from './Style/RegisterStyles.js';
+import {createUser} from '../lib/user.js';
 function SignUpScreen({navigation}) {
-  const [isRegister, setRegister] = useState(true);
   const [form, setForm] = useState({
     email: '',
     password: '',
@@ -27,9 +27,16 @@ function SignUpScreen({navigation}) {
   };
 
   const handleRegister = async () => {
-    // Check if email, password, confirmPassword are empty
+    //Check the email is school mail
+    if (!form.email.includes('sch.ac.kr')) {
+      Alert.alert(
+        '이메일 오류',
+        '순천향대학교 이메일을 사용해 주세요.(@sch.ac.kr)',
+      );
+      return;
+    }
     if (!form.email || !form.password || !form.confirmPassword) {
-      console.log('error!');
+      // Check if email, password, confirmPassword are empty
       Alert.alert('입력 오류', '모든 필드를 입력해 주세요.');
       return;
     }
@@ -46,6 +53,12 @@ function SignUpScreen({navigation}) {
         email,
         password,
       );
+      createUser({
+        email: user.email,
+        user_name: form.user_name,
+        user_id: form.user_id,
+        src_name: form.src_name,
+      });
       console.log('User account created with email: ', user.email);
       Alert.alert('회원가입이 완료되었습니다.');
       navigation.navigate('로그인');
@@ -60,42 +73,61 @@ function SignUpScreen({navigation}) {
   };
 
   return (
-    <View style={styles.input}>
-      <TextInput
-        placeholder="이름"
-        onChangeText={value => handleInputChange('user_name', value)}
-      />
-      <TextInput
-        placeholder="학번"
-        onChangeText={value => handleInputChange('user_id', value)}
-      />
-      <TextInput
-        placeholder="사용할 이메일 주소"
-        keyboardType="email-address"
-        onChangeText={value => handleInputChange('email', value)}
-      />
-      <TextInput
-        placeholder="비밀번호 입력"
-        secureTextEntry
-        onChangeText={value => handleInputChange('password', value)}
-      />
-      <TextInput
-        placeholder="비밀번호 재입력"
-        secureTextEntry
-        onChangeText={value => handleInputChange('confirmPassword', value)}
-      />
-      <TextInput
-        placeholder="거주생활관"
-        onChangeText={value => handleInputChange('src_name', value)}
-      />
-      <Button title="회원가입" onPress={handleRegister} />
+    <View style={styles.subContainer}>
+      <View style={styles.deepContainer}>
+        <Text style={{color: 'black'}}>*이름/학번은 ID 찾기에 사용됩니다.</Text>
+        <TextInput
+          placeholderTextColor={'#A9A9A9'}
+          style={styles.textInput}
+          placeholder="이름"
+          onChangeText={value => handleInputChange('user_name', value)}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="학번"
+          keyboardType="numeric"
+          placeholderTextColor={'#A9A9A9'}
+          onChangeText={value => handleInputChange('user_id', value)}
+        />
+      </View>
+
+      <View style={styles.deepContainer}>
+        <Text style={{color: 'black'}}>*이메일 주소는 ID로 사용됩니다.</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholderTextColor={'#A9A9A9'}
+          placeholder="사용할 이메일 주소"
+          keyboardType="email-address"
+          onChangeText={value => handleInputChange('email', value)}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholderTextColor={'#A9A9A9'}
+          placeholder="비밀번호 입력"
+          secureTextEntry
+          onChangeText={value => handleInputChange('password', value)}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholderTextColor={'#A9A9A9'}
+          placeholder="비밀번호 재입력"
+          secureTextEntry
+          onChangeText={value => handleInputChange('confirmPassword', value)}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="거주생활관 (예: 향설3관)"
+          placeholderTextColor={'#A9A9A9'}
+          onChangeText={value => handleInputChange('src_name', value)}
+        />
+      </View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity onPress={handleRegister} style={styles.button}>
+          <Text style={styles.buttonText}>회원가입</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  input: {
-    backgroundColor: 'black',
-    color: 'red',
-  },
-});
+
 export default SignUpScreen;
